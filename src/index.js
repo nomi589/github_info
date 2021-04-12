db7179fa597ea2bc4ea99b7aa156ca9b5626d670
Noman Rauf <nomi589@hotmail.com>
2021-04-12 12:10:31 +0300

    Configure webpack with babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("working");
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+function App() {
+  const [value, setValue] = React.useState(5);
+
+  return <h1>{value}</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
