465bbd5eb2b7be38e656ed3e8fd345f25e7f2a2e
Noman Rauf <nomi589@hotmail.com>
2021-04-13 11:32:08 +0300

    Configure react router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import Repos from "./components/Repos";
+import User from "./components/User";
 
 import "./index.css";
 
-function App() {
-  const [value, setValue] = React.useState(5);
+function NavLinks() {
+  return (
+    <ul>
+      <li>
+        <NavLink to="/" activeClassName="navlink-active">
+          User Info
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/repos" activeClassName="navlink-active">
+          Repos Info
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
 
-  return <h1>{value}</h1>;
+function App() {
+  return (
+    <Router>
+      <NavLinks />
+
+      <Route path="/" exact component={User} />
+      <Route path="/repos" component={Repos} />
+    </Router>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
