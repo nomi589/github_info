1b453e14f7a2d20997fdc5e0d7c5b0b218ceefb5
Noman Rauf <nomi589@hotmail.com>
2021-04-14 17:35:32 +0300

    Add styles for user input form

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { getUser } from "../helpers/api";
 
+import "./styles/user.css";
+
 export default function User() {
   const [user, setUser] = React.useState("");
   const [userData, setUserData] = React.useState(null);
@@ -8,7 +10,6 @@ export default function User() {
   const handleSubmit = (e) => {
     console.log("submitted");
     getUser(user).then((userData) => {
-      console.log(userData);
       setUserData(userData);
     });
   };
@@ -24,7 +25,7 @@ export default function User() {
         <input type="button" value="Get user info" onClick={handleSubmit} />
       </section>
       <section className="user-display">
-        {userData && JSON.stringify(userData, null, 2)}
+        {userData && <pre>{JSON.stringify(userData, null, 2)}</pre>}
       </section>
     </React.Fragment>
   );
