178a092ff7ad7078158800d39510899f20de3435
Noman Rauf <nomi589@hotmail.com>
2021-04-15 14:12:29 +0300

    Configure to deploy on Netlify
    Install webpack plugin 'copy-webpack-plugin' and create the _redirects
    file required by Netlify. Add redirects to _redirects file so all 404s
    are redirected to index.html (React Router takes over from there).
    Lastly, use the copy-webpack-plugin in webpack.prod.js so the _redirects
    file is copied to the final dist folder.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -17,6 +18,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: path.resolve(__dirname, "_redirects") }],
     }),
   ],
 };
