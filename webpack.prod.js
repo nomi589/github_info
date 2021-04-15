54665ce296301609e99b1287fc42dc8313c52177
Noman Rauf <nomi589@hotmail.com>
2021-04-15 14:04:16 +0300

    Configure files for production
    Create separate webpack config files for dev (webpack.dev.js) and prod
    (webpack.prod.js) and update the npm scripts to use the appropriate file

---
+++ webpack.prod.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      { test: /\.m?js$/, use: "babel-loader" },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
