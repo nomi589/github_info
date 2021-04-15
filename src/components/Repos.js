b23283322ce9b1619e2ff13946773403dc4428ef
Noman Rauf <nomi589@hotmail.com>
2021-04-15 13:03:59 +0300

    Fetch and filter repos data from api

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { getRepos } from "../helpers/api";
+import InputForm from "./InputForm";
 
 export default function Repos() {
-  return <p>Repos</p>;
+  const [repoInput, setRepoInput] = React.useState("");
+  const [reposData, setReposData] = React.useState(null);
+
+  const handleInput = (e) => {
+    setRepoInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    console.log("submitted");
+    getRepos(repoInput).then((repos) => {
+      setReposData(repos);
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <InputForm
+        placeholder="enter a language name e.g. 'javascript'..."
+        handleInput={handleInput}
+        handleSubmit={handleSubmit}
+        searchBtnDisabled={!repoInput}
+      />
+      {reposData && <pre>{JSON.stringify(reposData, null, 2)}</pre>}
+    </React.Fragment>
+  );
 }
--- src/helpers/api.js
+++ src/helpers/api.js
@@ -20,3 +20,28 @@ export function getUsers(user) {
       throw new Error(error);
     });
 }
+
+export function getRepos(language) {
+  const queryEndpoint = `https://api.github.com/search/repositories?q=language=${language}&sort=stars&order=desc`;
+
+  return fetch(queryEndpoint)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      const repos = data.items;
+
+      return repos.map((repo) => {
+        return {
+          title: repo.full_name,
+          url: repo.html_url,
+          avatar: repo.owner.avatar_url,
+          owner: repo.owner.login,
+          owner_url: repo.owner.html_url,
+        };
+      });
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+}
